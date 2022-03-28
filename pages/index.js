c0d8dc30373a499e0e3c0a65b19bf8a54543ad83
kelvim_Santos <contatoshake@hotmail.com>
2022-03-27 21:36:16 -0300

    outrabranch  commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import{useState} from 'react';
 function Home()
 {
     return<div>
-    <h1>MINHA PÁGINA</h1>
+    <h1>MINHA PÁGINA segunda branch</h1>
             <Contador/>
             <div>seja bem vindo!</div>
             </div> 
